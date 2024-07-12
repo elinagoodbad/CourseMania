02933c37e5e4b4bf776024df34feb8897c7b54fe
Bekjan Akparaliev <beka152005@gmail.com>
2024-07-12 16:40:40 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/homePage/Navbar";
+import Header from "./components/homePage/Header";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
---
+++ src/components/homePage/Header.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Typography, Container } from "@mui/material";
+import styles from "./Header.module.css";
+
+const images = [
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+];
+
+const Header = () => {
+  return (
+    <Box className={styles.headerContainer}>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Преподаватель ${index + 1}`}
+          className={styles.headerImage}
+          style={{ left: `${index * 20}%` }}
+        />
+      ))}
+      <Container className={styles.headerText}>
+        <Typography variant="h1" className={styles.headerTitle}>
+          <span className={styles.bold}>U! SKILLZ</span>
+        </Typography>
+        <Typography variant="h4" className={styles.headerSubtitle}>
+          Школа креативных профессий
+        </Typography>
+      </Container>
+    </Box>
+  );
+};
+
+export default Header;
