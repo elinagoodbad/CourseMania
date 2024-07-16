e4f036d88b8fbd106ce765f7bae8d8a032a22021
Bekjan1337 <167067494+Bekjan1337@users.noreply.github.com>
2024-07-17 00:49:39 +0600

    Update Header.jsx

--- src/components/homePage/Header.jsx
+++ src/components/homePage/Header.jsx
@@ -3,9 +3,9 @@ import { Box, Typography, Container } from "@mui/material";
 import styles from "./Header.module.css";
 
 const images = [
-  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/7/78/Vasily_Perov_-_%D0%9F%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82_%D0%A4.%D0%9C.%D0%94%D0%BE%D1%81%D1%82%D0%BE%D0%B5%D0%B2%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_-_Google_Art_Project.jpg",
+  "https://sun9-24.userapi.com/impg/63ScMY3E00CR_WNVf6KFjqpSrmbdzPLCSiE_6w/-9YID7BJKVU.jpg?size=1024x1280&quality=96&sign=7322958897bc32f71098e1c225fbcf9b&type=album",
+  "https://sun9-63.userapi.com/impg/q5UowM-s-ybsatR1unUSVUXai8DewMOCoD9kDQ/pXjrdjWWXOs.jpg?size=666x1024&quality=96&sign=1bf9bea03712f53b2b4c849a94189c7c&type=album",
+  "https://sun9-43.userapi.com/impg/PR0i0QDzYf-aDFwP2FhRKs0bAE687whi4hYbKA/uDfhsUr34Pk.jpg?size=640x640&quality=96&sign=31fadcd5c9a79023570a49fa750e4736&type=album",
 ];
 
 const Header = () => {
