206cd037828cd4c09d79aa38f5bab13c9604a64d
Bekjan1337 <167067494+Bekjan1337@users.noreply.github.com>
2024-07-16 20:59:36 +0600

    Update ProductCard.jsx

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -4,9 +4,11 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import styles from "./ProductCard.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ elem }) => {
   const { deleteProduct } = useProduct();
+  const navigate = useNavigate();
 
   return (
     <div className={styles.productCard}>
@@ -16,12 +18,16 @@ const ProductCard = ({ elem }) => {
       <div className={styles.content}>
         <h2 className={styles.slug}>{elem.slug}</h2>
         <div className={styles.iconGroup}>
-          <IconButton aria-label="edit" className={styles.iconButton}>
+          <IconButton
+            aria-label="edit"
+            onClick={() => navigate(`/edit/${elem.id}`)}
+            className={styles.iconButton}
+          >
             <EditIcon />
           </IconButton>
           <IconButton
             aria-label="delete"
-            onClick={() => deleteProduct(elem.slug)}
+            onClick={() => deleteProduct(elem.id)}
             className={styles.iconButton}
           >
             <DeleteIcon />
